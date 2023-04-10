40afe9eb2134501693b5f0cf5fdc064ca14d3f62
mikael.quadros <mikael.quadros@gmail.com>
2023-04-10 00:24:06 -0300

    Hello world frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListTax from "./pages/tax";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// @ts-ignore
+import { GlobalStyle } from './styles/global';
+
+export default function App() {
+    // @ts-ignore
+    return (
+        <>
+            <ListTax />
+            <GlobalStyle/>
+        </>
+    );
 }
-
-export default App;
